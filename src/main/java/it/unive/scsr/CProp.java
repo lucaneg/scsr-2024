f16c5d2b3e8143b347590aa071bf4cd197622f23
ErBerta <elia@bertapelle.it>
2024-03-04 21:49:18 +0100

    Task 1

--- src/main/java/it/unive/scsr/CProp.java
+++ src/main/java/it/unive/scsr/CProp.java
@@ -4,114 +4,126 @@ import it.unive.lisa.analysis.ScopeToken;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.dataflow.DataflowElement;
 import it.unive.lisa.analysis.dataflow.DefiniteDataflowDomain;
-import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.ProgramPoint;
 import it.unive.lisa.symbolic.SymbolicExpression;
 import it.unive.lisa.symbolic.value.*;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
 import it.unive.lisa.util.representation.ListRepresentation;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
+import java.util.*;
 public class CProp implements DataflowElement<DefiniteDataflowDomain<CProp>, CProp> {
 
 	private final Identifier id;
 	private final Integer constant;
-	//private final Constant constant;
 
 	public CProp() {
 		this(null, null);
 	}
-	public CProp(Identifier id, Integer constant){
+
+	public CProp(Identifier id, Integer constant) {
 		this.id = id;
-		//this.constant = 0;
 		this.constant = constant;
 	}
 
-	public Integer calculateValue(SymbolicExpression exp, DefiniteDataflowDomain<CProp> domain) {
+	private Integer calculateValue(SymbolicExpression exp, DefiniteDataflowDomain<CProp> domain) {
 		Integer result = null;
 
-		switch(exp){
-			case Constant c:
-				Object cTemp = c.getValue();
-				if (cTemp instanceof Integer)
-					result = (Integer) cTemp;
-				break;
-			case Identifier i:
-				break;
-			case UnaryExpression ue:
-				result = calculateValue(ue.getExpression(), domain);
-				break;
-			case BinaryExpression be:
-				break;
-            default:
-                result = null;
-				break;
-        }
+		// If exp is a Constant, then the result is it's value
+		if (exp instanceof Constant) {
+			Constant c = (Constant) exp;
+
+			if (c.getValue() instanceof Integer) {
+				result = (Integer) c.getValue();
+			}
+		// If exp is an Indentifier, then i have to search for the result
+		} else if (exp instanceof Identifier) {
+			Identifier id = (Identifier) exp;
+
+			for (CProp cp : domain.getDataflowElements()) {
+				if (cp.id.equals(id)) {
+					result = cp.constant;
+					break;
+				}
+			}
+		// If exp is a Unary Expression, then I have to extract expression and domain
+		} else if (exp instanceof UnaryExpression) {
+			UnaryExpression ue = (UnaryExpression) exp;
+			result = calculateValue(ue.getExpression(), domain);
+		// If exp is a BinaryExpression, I have to split it into its main parts, left and right Integer and the BinaryOperator
+		} else if (exp instanceof BinaryExpression) {
+			BinaryExpression be = (BinaryExpression) exp;
+			Integer left = calculateValue(be.getLeft(), domain);
+			BinaryOperator bp = be.getOperator();
+			Integer right = calculateValue(be.getRight(), domain);
+
+			if (left == null || right == null)
+				result = null;
+			else if (bp instanceof AdditionOperator)
+				result = left + right;
+			else if (bp instanceof SubtractionOperator)
+				result = left - right;
+			else if (bp instanceof MultiplicationOperator)
+				result = left * right;
+			else if (bp instanceof ModuloOperator)
+				result = left % right;
+			else if (bp instanceof DivisionOperator)
+				result = right == 0 ? null : left / right;
+		}
+
 		return result;
 	}
+
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		CProp cProp = (CProp) o;
-		return Objects.equals(id, cProp.id) && Objects.equals(constant, cProp.constant);
+	public Collection<Identifier> getInvolvedIdentifiers() {
+		return Collections.singleton(id);
+	}
+
+	@Override
+	public Collection<CProp> gen(Identifier id, ValueExpression exp, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		Collection<CProp> cp;
+		if(calculateValue(exp, domain) != null)
+			cp = Collections.singleton(new CProp(id, constant));
+		else
+			cp = Collections.emptySet();
+
+		return  cp;
+	}
+
+	@Override
+	public Collection<CProp> gen(ValueExpression exp, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Collection<CProp> kill(Identifier id, ValueExpression exp, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		Set<CProp> elements = domain.getDataflowElements();
+		Optional<CProp> cp = elements.stream().filter(x -> x.id.equals(id)).findFirst();
+		return cp.map(Collections::singleton).orElse(Collections.emptySet());
+	}
+
+	@Override
+	public Collection<CProp> kill(ValueExpression exp, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+
+		CProp other = (CProp) obj;
+
+		return (!Objects.equals(this.id, other.id) || !Objects.equals(this.constant, other.constant));
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, constant);
 	}
-
-	@Override
-	public Collection<Identifier> getInvolvedIdentifiers() {
-		Set<Identifier> result = new HashSet<>();
-		result.add(id);
-		return result;
-	}
-
-	@Override
-	public Collection<CProp> gen(Identifier identifier, ValueExpression valueExpression, ProgramPoint pp, DefiniteDataflowDomain<CProp> cPropPossibleDataflowDomain) throws SemanticException {
-		// we generate a new element tracking this definition
-		Set<CProp> result = new HashSet<>();
-		CProp rd = new CProp(id,constant);
-		result.add(rd);
-		return result;
-	}
-
-	@Override
-	public Collection<CProp> gen(ValueExpression valueExpression, ProgramPoint programPoint, DefiniteDataflowDomain<CProp> cPropPossibleDataflowDomain) throws SemanticException {
-		return null;
-	}
-
-	@Override
-	public Collection<CProp> kill(Identifier identifier, ValueExpression valueExpression, ProgramPoint programPoint, DefiniteDataflowDomain<CProp> domain) throws SemanticException {
-		Set<CProp> killed = new HashSet<>();
-		for (CProp cp : domain.getDataflowElements())
-			// we could use `rd.variable.equals(id)` as elements of this class
-			// refer to one variable at a time
-			if (cp.getInvolvedIdentifiers().contains(id))
-				killed.add(cp);
-		return killed;
-	}
-
-	@Override
-	public Collection<CProp> kill(ValueExpression valueExpression, ProgramPoint programPoint, DefiniteDataflowDomain<CProp> cPropPossibleDataflowDomain) throws SemanticException {
-		return new HashSet<>();
-	}
-
-	// IMPLEMENTATION NOTE:
-	// the code below is outside of the scope of the course. You can uncomment
-	// it to get your code to compile. Be aware that the code is written
-	// expecting that a field named "id" and a field named "constant" exist
-	// in this class: if you name them differently, change also the code below
-	// to make it work by just using the name of your choice instead of
-	// "id"/"constant". If you don't have these fields in your
-	// solution, then you should make sure that what you are doing is correct :)
 
 	@Override
 	public StructuredRepresentation representation() {
