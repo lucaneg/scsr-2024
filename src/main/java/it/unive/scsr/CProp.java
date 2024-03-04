314e6e1f2cfa84c2572c069d4c9799c9189c3f46
Fede934 <fe.carrar@gmail.com>
2024-03-04 20:29:26 +0100

    Modify Cprop

--- src/main/java/it/unive/scsr/CProp.java
+++ src/main/java/it/unive/scsr/CProp.java
@@ -1,6 +1,23 @@
 package it.unive.scsr;
 
-public class CProp {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.analysis.dataflow.DefiniteDataflowDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+public class CProp implements DataflowElement<DefiniteDataflowDomain<CProp>, CProp>  {
 
 	// IMPLEMENTATION NOTE:
 	// the code below is outside of the scope of the course. You can uncomment
@@ -11,24 +28,112 @@ public class CProp {
 	// "id"/"constant". If you don't have these fields in your
 	// solution, then you should make sure that what you are doing is correct :)
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		return new ListRepresentation(
-//				new StringRepresentation(id), 
-//				new StringRepresentation(constant));
-//	}
-//
-//	@Override
-//	public CProp pushScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
-//
-//	@Override
-//	public CProp popScope(
-//			ScopeToken scope)
-//			throws SemanticException {
-//		return this;
-//	}
+	private Identifier id;
+	private Constant constant;
+	
+	public CProp()
+	{
+		
+		id=null;
+		constant=null;
+	}
+	
+	public CProp(Identifier id,Constant constant)
+	{
+		this.id=id;
+		this.constant=constant;
+		
+	}
+	
+		@Override
+	public int hashCode() {
+		return Objects.hash(constant, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CProp other = (CProp) obj;
+		return Objects.equals(constant, other.constant) && Objects.equals(id, other.id);
+	}
+
+
+	@Override
+	public Collection<Identifier> getInvolvedIdentifiers() {
+		
+		Set<Identifier> result = new HashSet<>();
+		result.add(id);
+		return result;
+	}
+
+	@Override
+	public Collection<CProp> gen(Identifier id, ValueExpression expression, ProgramPoint pp,
+			DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		
+		Set<CProp> constants= new HashSet<>();
+		for (CProp cp: domain.getDataflowElements())
+		{
+			constants.add(cp);
+			System.out.println("Expression: "+ expression.getStaticType());
+		}
+		return constants;
+	}
+
+	@Override
+	public Collection<CProp> gen(ValueExpression expression, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+			throws SemanticException {
+		
+		return new HashSet<>(); /*empty set*/
+	}
+
+	@Override
+	public Collection<CProp> kill(Identifier id, ValueExpression expression, ProgramPoint pp,
+			DefiniteDataflowDomain<CProp> domain) throws SemanticException {
+		
+		Set<CProp> killed=new HashSet<>();
+		
+		for (CProp cp: domain.getDataflowElements())
+		{	
+			if(cp.getInvolvedIdentifiers().contains(id))
+			killed.add(cp);
+			
+		}
+		
+		return killed;
+	}
+
+	@Override
+	public Collection<CProp> kill(ValueExpression expression, ProgramPoint pp, DefiniteDataflowDomain<CProp> domain)
+			throws SemanticException {
+		
+		return new HashSet<>(); /*empty set*/
+	}
+	
+	@Override
+	public StructuredRepresentation representation() {
+		return new ListRepresentation(
+				new StringRepresentation(id), 
+				new StringRepresentation(constant));
+	}
+
+	@Override
+	public CProp pushScope(
+			ScopeToken scope)
+			throws SemanticException {
+		return this;
+	}
+
+	@Override
+	public CProp popScope(
+			ScopeToken scope)
+			throws SemanticException {
+		return this;
+	}
+
+
 }
