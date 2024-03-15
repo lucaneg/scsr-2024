589e1c11eb452948898b888aa241d9922b30244e
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-03-15 12:09:29 +0100

    Comments update

--- src/main/java/it/unive/scsr/Parity.java
+++ src/main/java/it/unive/scsr/Parity.java
@@ -31,20 +31,26 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 	 * ODD = { x | x is ODD }    ex: {-4}{-2}{0}{2}{4}...
 	 */
 	
-	// I defined four Parity elements:
-	// - BOTTOM which comprehends the empty element or if an element has an error
-	// - EVEN which is associated to the even numbers
-	// - ODD which is associated to the odd numbers
-	// - TOP which comprehends the sub-levels EVEN and ODD
+	/*
+	 * I defined four Parity elements:
+	 * - BOTTOM which comprehends the empty element or if an element has an error
+	 * - EVEN which is associated to the even numbers
+	 * - ODD which is associated to the odd numbers
+	 * - TOP which comprehends the sub-levels EVEN and ODD
+	 */
 	private static final Parity BOTTOM = new Parity(-10);
 	private static final Parity EVEN = new Parity(-1);
 	private static final Parity ODD = new Parity(0);
 	private static final Parity TOP = new Parity(10);
 	
-	// This is just needed to distinguish the elements
+	/*
+	 * This is just needed to distinguish the elements
+	 */
 	private final int parity;
 	
-	// Constructors
+	/*
+	 * Constructors
+	 */
 	public Parity() {
 		this(10);
 	}
@@ -59,7 +65,11 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		return Objects.hash(parity);
 	}
 
-	// This method evaluates if two objects are equal so if they represent the same Parity value
+	/* 
+	 * This method evaluates if two objects are equal so if they represent the same Parity value
+	 * @input: an Object obj to compare with the Object from which the function is called
+ 	 * @return: boolean value representing if the two objects are equal
+ 	*/
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -72,12 +82,16 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		return parity == other.parity;
 	}
 	
-	// In this function I find the least upper bound of the two Parity elements, the one which call the function and the one passed as parameter
-	// I considered four main cases:
-	// 1. if both elements are EVEN the least upper bound is EVEN
-	// 2. if both elements are ODD the least upper bound is ODD
-	// 3. if both elements are BOTTOM the least upper bound is BOTTOM
-	// 4. otherwise, if the elements are different (so one EVEN and the other ODD, or vice versa, or one BOTTOM and the other EVEN/ODD, or vice versa) the least upper bound is TOP
+	/* 
+	 * In this function I find the least upper bound of the two Parity elements, the one which call the function and the one passed as parameter
+	 * I considered four main cases:
+	 * 1. if both elements are EVEN the least upper bound is EVEN
+	 * 2. if both elements are ODD the least upper bound is ODD
+	 * 3. if both elements are BOTTOM the least upper bound is BOTTOM
+	 * 4. otherwise, if the elements are different (so one EVEN and the other ODD, or vice versa, or one BOTTOM and the other EVEN/ODD, or vice versa) the least upper bound is TOP
+	 * @input: a Parity element to compare with the element from which the function is called to determine the least upper bound
+	 * @return: a Parity element representing the least upper bound of the two elements in input
+	*/
 	@Override
 	public Parity lubAux(Parity other) throws SemanticException {
 		if (this == EVEN && other == EVEN)
@@ -89,16 +103,20 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		else return TOP;
 	}
 	
-	// This function evaluates if, given two elements (the one which call the function and the one passed as parameter)
-	// if the first, so this is less or equal than other
-	// I considered seven cases:
-	// 1. if both elements are TOP the function returns true
-	// 2. if both elements are BOTTOM the function returns true
-	// 3. if one element (this) equals TOP while the other isn't equal to TOP nor BOTTOM the function returns false because the TOP element is greater than the others
-	// 4. if one element (other) equals TOP while the other isn't equal to TOP nor BOTTOM the function returns true because the TOP element is greater than the others
-	// 5. if one element (this) equals BOTTOM while the other isn't equal to TOP nor BOTTOM the function returns false because the TOP element is greater than the others
-	// 6. if one element (other) equals BOTTOM while the other isn't equal to TOP nor BOTTOM the function returns true because the TOP element is greater than the others
-	// 7. if no one element equals TOP the function pass the evaluation to the lessOrEqual function
+	/* 
+	 * This function evaluates if, given two elements (the one which call the function and the one passed as parameter)
+	 * if the first, so this is less or equal than other
+	 * I considered seven cases:
+	 * 1. if both elements are TOP the function returns true
+	 * 2. if both elements are BOTTOM the function returns true
+	 * 3. if one element (this) equals TOP while the other isn't equal to TOP nor BOTTOM the function returns false because the TOP element is greater than the others
+	 * 4. if one element (other) equals TOP while the other isn't equal to TOP nor BOTTOM the function returns true because the TOP element is greater than the others
+	 * 5. if one element (this) equals BOTTOM while the other isn't equal to TOP nor BOTTOM the function returns false because the TOP element is greater than the others
+	 * 6. if one element (other) equals BOTTOM while the other isn't equal to TOP nor BOTTOM the function returns true because the TOP element is greater than the others
+	 * 7. if no one element equals TOP the function pass the evaluation to the lessOrEqual function
+	 * @input: a Parity element to compare with the element from which the function is called to determine the less or equal relation
+	 * @return: a boolean element that says if the first element is less or equal than the second
+	*/
 	@Override
 	public boolean lessOrEqualAux(Parity other) throws SemanticException {
 	    if (this == TOP && other == TOP)
@@ -117,7 +135,9 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 	        return this.lessOrEqual(other);
 	}
 
-	//This functions returns TOP and BOTTOM unconditionally
+	/*
+	 * This functions returns TOP and BOTTOM unconditionally
+	 */
 	@Override
 	public Parity top() {
 		return TOP;
@@ -128,11 +148,15 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		return BOTTOM;
 	}
 	
-	// This function evaluates constants considering two cases
-	// It just check if the constant is divisible by two and if it isn't
-	// In the positive case the number is EVEN 
-	// (in this case I also check if the number equals zero because zero is even but dividing it by zero it could return error so it would become BOTTOM)
-	// In the other case the number is ODD
+	/*
+	 * This function evaluates constants considering two cases
+	 * It just check if the constant is divisible by two and if it isn't
+	 * In the positive case the number is EVEN 
+	 * (in this case I also check if the number equals zero because zero is even but dividing it by zero it could return error so it would become BOTTOM)
+	 * In the other case the number is ODD
+	 * @input: a constant, variable whose value cannot be changed after it has been initialized, a program point, specific location in a program where the execution can reach, a semantic oracle, a tool or a component used in program analysis to provide information about the semantics of a program.
+	 * @return: a Parity element representing the evaluation of the constant
+	 */
 	@Override
 	public Parity evalNonNullConstant(
 			Constant constant,
@@ -149,7 +173,10 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		return top();
 	}
 	
-	// This function evaluates the negation of the number, so it changes the sign but it doesn't change the Parity value
+	/*
+	 * This function evaluates the negation of the number, so it changes the sign but it doesn't change the Parity value
+	 * @return: the negation of the element from which the function is called
+	 */
 	private Parity negate() {
 		if (this == EVEN)
 			return EVEN;
@@ -159,7 +186,11 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 			return this;
 	}
 	
-	// This function evaluates the unary expressions (so the negation operator)
+	/*
+	 * This function evaluates the unary expressions (so the negation operator)
+	 * @input: a constant, variable whose value cannot be changed after it has been initialized, a program point, specific location in a program where the execution can reach, a semantic oracle, a tool or a component used in program analysis to provide information about the semantics of a program.
+	 * @return: the evaluation of the element from which the function is called
+	 */
 	public Parity evalUnaryExpression(
 			UnaryOperator operator,
 			Parity arg,
@@ -176,10 +207,14 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 		return TOP;
 	}
 
-	// This function evaluates the binary expressions such as addition, subtraction and multiplication
-	// First I check if the elements are TOP or BOTTOM
-	// If not I evaluate the operator:
-	// I evaluated mainly three operations: addition, subtraction and multiplication (division is not required, but in case of division the function returns TOP)
+	/*
+	 * This function evaluates the binary expressions such as addition, subtraction and multiplication
+	 * First I check if the elements are TOP or BOTTOM
+	 * If not I evaluate the operator:
+	 * I evaluated mainly three operations: addition, subtraction and multiplication (division is not required, but in case of division the function returns TOP)
+	 * @input: a constant, variable whose value cannot be changed after it has been initialized, a program point, specific location in a program where the execution can reach, a semantic oracle, a tool or a component used in program analysis to provide information about the semantics of a program.
+	 * @return: the evaluation of the result of the binary expression
+	 */
 	public Parity evalBinaryExpression(
 			BinaryOperator operator,
 			Parity left,
@@ -230,7 +265,7 @@ public class Parity implements BaseNonRelationalValueDomain<Parity> {
 	// change also the code below to make it work by just using the name of your
 	// choice. If you use methods instead of constants, change == with the
 	// invocation of the corresponding method
-
+	
 	@Override
 	public StructuredRepresentation representation() {
 		if (this == TOP)
