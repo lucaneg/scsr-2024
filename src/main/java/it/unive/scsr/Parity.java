edb2c5e9725d1e3f90ca32713d3497112e3e80ee
Matteo Grazioso <884055@stud.unive.it>
2024-03-17 16:46:25 +0100

    Second Task Solution

--- src/main/java/it/unive/scsr/Parity.java
+++ src/main/java/it/unive/scsr/Parity.java
@@ -1,6 +1,135 @@
 package it.unive.scsr;
 
-public class Parity {
+import it.unive.lisa.analysis.Lattice;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+public class Parity implements BaseNonRelationalValueDomain<Parity>{
+
+	public static final Parity EVEN = new Parity(2);
+	public static final Parity ODD = new Parity(3);
+	public static final Parity TOP = new Parity(1);
+	public static final Parity BOTTOM = new Parity(0);
+	
+	private final int parity;
+
+	public Parity() {
+		this.parity = 1;
+	}
+
+	private Parity(int parity) {
+		this.parity = parity;
+	}
+
+	@Override
+	public Parity top() {
+		return TOP;
+	}
+
+	@Override
+	public Parity bottom() {
+		return BOTTOM;
+	}
+
+	@Override
+	public Parity lubAux(Parity o) throws SemanticException {
+		return TOP;
+	}
+
+	@Override
+	public boolean lessOrEqualAux(Parity o) throws SemanticException {
+		return false;
+	}
+
+	@Override
+	public Parity evalNullConstant(ProgramPoint pp, SemanticOracle oracle) {
+		return top();
+	}
+
+	@Override
+	public Parity evalNonNullConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) {
+		Object value = constant.getValue();
+		
+		if (value instanceof Integer) {
+			Integer i = (Integer) value;
+			if (i % 2 == 0)
+				return EVEN;
+			return ODD;
+		}
+
+		return top();
+	}
+
+	@Override
+	public Parity evalUnaryExpression(UnaryOperator operator, Parity arg, ProgramPoint pp, SemanticOracle oracle) {
+		if (operator == NumericNegation.INSTANCE)
+			return arg;
+		return top();
+	}
+
+	public boolean isEven() {
+		return this == EVEN;
+	}
+
+	public boolean isOdd() {
+		return this == ODD;
+	}
+
+	@Override
+	public Parity evalBinaryExpression(BinaryOperator operator, Parity left, Parity right, ProgramPoint pp, SemanticOracle oracle) {
+		if (left.isTop()) 		return top();
+		else if (right.isTop()) return top();
+		
+		if (operator instanceof AdditionOperator) {
+			if (right.equals(left)) return EVEN;
+			else 					return ODD;
+		}
+		else if (operator instanceof SubtractionOperator) {
+			if (right.equals(left)) return EVEN;
+			else 					return ODD;
+		}
+		else if (operator instanceof MultiplicationOperator)
+			if (left.isEven() || right.isEven()) return EVEN;
+			else								 return ODD;
+		else if (operator instanceof DivisionOperator)
+			return TOP;
+
+		return TOP;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + parity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Parity other = (Parity) obj;
+		if (parity != other.parity)
+			return false;
+		return true;
+	}
 
 	// IMPLEMENTATION NOTE:
 	// the code below is outside of the scope of the course. You can uncomment
@@ -11,14 +140,16 @@ public class Parity {
 	// choice. If you use methods instead of constants, change == with the
 	// invocation of the corresponding method
 
-//	@Override
-//	public StructuredRepresentation representation() {
-//		if (this == TOP)
-//			return Lattice.topRepresentation();
-//		if (this == BOTTOM)
-//			return Lattice.bottomRepresentation();
-//		if (this == EVEN)
-//			return new StringRepresentation("EVEN");
-//		return new StringRepresentation("ODD");
-//	}
+	@Override
+	public StructuredRepresentation representation() {
+		if (this == TOP)
+			return Lattice.topRepresentation();
+		if (this == BOTTOM)
+			return Lattice.bottomRepresentation();
+		if (this == EVEN)
+			return new StringRepresentation("EVEN");
+		return new StringRepresentation("ODD");
+	}
+
+	
 }
