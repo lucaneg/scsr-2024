ffcde76d1e97882c98b3b65070393ceea983f5f7
NicoleM00 <NicoleM00@users.noreply.github.com>
2024-03-19 15:50:40 +0100

    Parity

--- src/main/java/it/unive/scsr/Parity.java
+++ src/main/java/it/unive/scsr/Parity.java
@@ -1,5 +1,16 @@
 package it.unive.scsr;
 
+import it.unive.lisa.analysis.Lattice;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
 public class Parity {
 
 	// IMPLEMENTATION NOTE:
@@ -10,15 +21,110 @@ public class Parity {
 	// change also the code below to make it work by just using the name of your
 	// choice. If you use methods instead of constants, change == with the
 	// invocation of the corresponding method
-
-//	@Override
-//	public StructuredRepresentation representation() {
-//		if (this == TOP)
-//			return Lattice.topRepresentation();
-//		if (this == BOTTOM)
-//			return Lattice.bottomRepresentation();
-//		if (this == EVEN)
-//			return new StringRepresentation("EVEN");
-//		return new StringRepresentation("ODD");
-//	}
+	@Override
+	public StructuredRepresentation representation() {
+		if (this == TOP)
+			return Lattice.topRepresentation();
+		if (this == BOTTOM)
+			return Lattice.bottomRepresentation();
+		if (this == EVEN)
+			return new StringRepresentation("EVEN");
+		if (this == ODD)
+			return new StringRepresentation("ODD");
+	}
+	@Override
+	public Signs evalBinaryExpression(
+			BinaryOperator operator,
+			Signs left,
+			Signs right)
+			throws SemanticException {
+		if (operator instanceof AdditionOperator) {
+			if (left == ODD) {
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return EVEN;
+				else if (right == EVEN)
+					return ODD;
+				else
+					return BOTTOM;
+			} else if (left == EVEN) { // even
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return ODD;
+				else if (right == EVEN)
+					return EVEN;
+				else
+					return BOTTOM;
+			} else if (left == TOP) {	// top
+				if (right== TOP || right == EVEN || right == ODD)
+					return TOP;
+				else
+					return BOTTOM;
+			} else
+				return BOTTOM;	// bottom
+		} else if (operator instanceof SubtractionOperator) {
+			if (left == ODD) {
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return EVEN;
+				else if (right == EVEN)
+					return ODD;
+				else
+					return BOTTOM;
+			} else if (left == EVEN) { // even
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return ODD;
+				else if (right == EVEN)
+					return EVEN;
+				else
+					return BOTTOM;
+			} else if (left == TOP) {	// top
+				if (right== TOP || right == EVEN || right == ODD)
+					return TOP;
+				else
+					return BOTTOM;
+			} else
+				return BOTTOM;	// bottom
+		} else if (operator instanceof MultiplicationOperator) {
+			if (left == ODD) {
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return ODD;
+				else if (right == EVEN)
+					return EVEN;
+				else
+					return BOTTOM;
+			} else if (left == EVEN) { // even
+				if (right == TOP)
+					return TOP;
+				else if (right == ODD)
+					return EVEN;
+				else if (right == EVEN)
+					return EVEN;
+				else
+					return BOTTOM;
+			} else if (left == TOP) {	// top
+				if (right== TOP || right == EVEN || right == ODD)
+					return TOP;
+				else
+					return BOTTOM;
+			} else
+				return BOTTOM;	// bottom
+		} else if (operator instanceof NegativeOperator) {
+			if (left == ODD)
+				return ODD;
+			else if (left == EVEN)
+				return EVEN;
+			else if (left == TOP)
+				return TOP;
+			else
+				return BOTTOM;
+	}
+}
 }
