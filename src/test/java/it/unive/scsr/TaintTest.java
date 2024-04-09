4dba2ec822c20572b600268b24cb41f7c9f92da2
HilinaBI <152439682+HilinaBI@users.noreply.github.com>
2024-04-09 23:36:40 +0200

    Update TaintTest.java

--- src/test/java/it/unive/scsr/TaintTest.java
+++ src/test/java/it/unive/scsr/TaintTest.java
@@ -1,7 +1,5 @@
 package it.unive.scsr;
 
-import org.junit.Test;
-
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
@@ -17,105 +15,65 @@ import it.unive.lisa.program.Program;
 import it.unive.lisa.program.Unit;
 import it.unive.lisa.program.cfg.CodeMember;
 import it.unive.lisa.program.cfg.Parameter;
-import it.unive.scsr.checkers.TaintChecker;
+import it.unive.scsr.checkers.DefiniteTaintChecker;
+
+import org.junit.Test;
 
 public class TaintTest {
-	
-	
-	
-	// we define the signatures for matching sources, sanitizers, and sinks
-	String[] sources = new String[] {"source1", "source2"};
-	String[] sanitizers = new String[] {"sanitizer1", "sanitizer2"};
-	String[] sinks = new String[] {"sink1", "sinks"};
-	
+    
+    // Define the signatures for matching sources, sanitizers, and sinks
+    String[] sources = new String[] {"source1", "source2"};
+    String[] sanitizers = new String[] {"sanitizer1", "sanitizer2"};
+    String[] sinks = new String[] {"sink1", "sinks"};
 
-	@Test
-	public void testTaint() throws ParsingException, AnalysisException {
-		// we parse the program to get the CFG representation of the code in it
-		Program program = IMPFrontend.processFile("inputs/taint.imp");
+    @Test
+    public void testDefiniteTaint() throws ParsingException, AnalysisException {
+        // Parse the program to get the CFG representation of the code in it
+        Program program = IMPFrontend.processFile("inputs/taint.imp");
 
-		// we load annotation for identify sources, sanitizer, and sinks during the analysis and checker execution
-		loadAnnotations(program);
-		
-		// we build a new configuration for the analysis
-		LiSAConfiguration conf = new DefaultConfiguration();
+        // Load annotations to identify sources, sanitizer, and sinks during the analysis and checker execution
+        loadAnnotations(program);
 
-		// we specify where we want files to be generated
-		conf.workdir = "outputs/taint";
+        // Build a new configuration for the analysis
+        LiSAConfiguration conf = new DefaultConfiguration();
 
-		// we specify the visual format of the analysis results
-		conf.analysisGraphs = GraphType.HTML;
-		
-		// we specify the create a json file containing warnings triggered by the analysis
-		conf.jsonOutput= true;
+        // Specify where files should be generated
+        conf.workdir = "outputs/taint";
 
-		// we specify the analysis that we want to execute
-		
-		 conf.abstractState = DefaultConfiguration.simpleState(
-				DefaultConfiguration.defaultHeapDomain(),
-				new ValueEnvironment<>(new Taint()),
-				DefaultConfiguration.defaultTypeDomain());
-		 
-		 // we specify to perform an interprocedural analysis (require to recognize calls to sources, sanitizers, and sinks)
-		 conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
-		 
-		 // the TaintChecker is executed after the Taint analysis and it checks if a tainted value is flowed in a sink
-		 conf.semanticChecks.add(new TaintChecker());
-		 
-		// we instantiate LiSA with our configuration
-		LiSA lisa = new LiSA(conf);
-		
+        // Specify the visual format of the analysis results
+        conf.analysisGraphs = GraphType.HTML;
 
-		// finally, we tell LiSA to analyze the program
-		lisa.run(program);
-	}
+        // Specify to create a JSON file containing warnings triggered by the analysis
+        conf.jsonOutput = true;
 
+        // Specify the analysis to execute
+        conf.abstractState = DefaultConfiguration.simpleState(
+                DefaultConfiguration.defaultHeapDomain(),
+                new ValueEnvironment<>(new DefiniteTaint()),
+                DefaultConfiguration.defaultTypeDomain());
 
-	private void loadAnnotations(Program program) {
-		
-		for(Unit unit : program.getUnits()) {
-			if(unit instanceof ClassUnit) {
-				ClassUnit cunit = (ClassUnit) unit;
-				for(CodeMember cm : cunit.getInstanceCodeMembers(false)) {
-					if(isSource(cm))
-						cm.getDescriptor().getAnnotations().addAnnotation(Taint.TAINTED_ANNOTATION);
-					else if(isSanitizer(cm)) 
-						cm.getDescriptor().getAnnotations().addAnnotation(Taint.CLEAN_ANNOTATION);
-					else if(isSink(cm))
-						for(Parameter param : cm.getDescriptor().getFormals()) {
-							param.addAnnotation(TaintChecker.SINK_ANNOTATION);
-						}		
-				}	
-			}
-		}
-		
-	}
+        // Specify to perform an interprocedural analysis (required to recognize calls to sources, sanitizers, and sinks)
+        conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
 
+        // The DefiniteTaintChecker is executed after the DefiniteTaint analysis and checks if a tainted value is flowed to a sink
+        conf.semanticChecks.add(new DefiniteTaintChecker());
 
-	private boolean isSource(CodeMember cm) {
-		for(String signatureName : sources) {
-			if(cm.getDescriptor().getName().equals(signatureName))
-				return true;
-		}
-		return false;
-	}
-	
+        // Instantiate LiSA with the provided configuration
+        LiSA lisa = new LiSA(conf);
 
-	private boolean isSanitizer(CodeMember cm) {
-		for(String signatureName : sanitizers) {
-			if(cm.getDescriptor().getName().equals(signatureName))
-				return true;
-		}
-		return false;
-	}
-	
+        // Finally, tell LiSA to analyze the program
+        lisa.run(program);
+    }
 
-	private boolean isSink(CodeMember cm) {
-		for(String signatureName : sinks) {
-			if(cm.getDescriptor().getName().equals(signatureName))
-				return true;
-		}
-		return false;
-	}
-	
-}
+    private void loadAnnotations(Program program) {
+        for (Unit unit : program.getUnits()) {
+            if (unit instanceof ClassUnit) {
+                ClassUnit cunit = (ClassUnit) unit;
+                for (CodeMember cm : cunit.getInstanceCodeMembers(false)) {
+                    if (isSource(cm))
+                        cm.getDescriptor().getAnnotations().addAnnotation(Taint.TAINTED_ANNOTATION);
+                    else if (isSanitizer(cm))
+                        cm.getDescriptor().getAnnotations().addAnnotation(Taint.CLEAN_ANNOTATION);
+                    else if (isSink(cm))
+                        for (Parameter param : cm.getDescriptor().getFormals()) {
+                            param.addAnnotation(Taint
